b8ceac98662feda3d3179846ed378d52c8b1acec
Michael Kessler <michi@netzpiraten.ch>
2013-11-27 16:41:06 +0100

    More CarrierWave configs.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,13 +5,17 @@ if Rails.env.test?
   end
 else
   CarrierWave.configure do |config|
+    config.root      = Rails.root.join('tmp')
+    config.cache_dir = 'carrierwave'
+
     config.fog_credentials = {
       :provider               => 'AWS',
       :aws_access_key_id      => 'xxx',
       :aws_secret_access_key  => 'yyy',
       :region                 => 'eu-west-1'
     }
-    config.fog_directory  = 'zzz'
+
+    config.fog_directory  = "#{ Rails.env }.smalljobs.ch"
     config.fog_public     = false
   end
 end
