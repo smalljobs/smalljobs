70493f89735f2f53a56594c5ce7d37818988aa0c
Michael Kessler <michi@netzpiraten.ch>
2013-11-29 23:23:00 +0100

    Add provider and seeker registration form.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def job_provider_contact_preferences
+    [
+      [I18n.t('contacts.phone'), 'phone' ],
+      [I18n.t('contacts.email'), 'email' ],
+      [I18n.t('contacts.mobile'), 'mobile' ]
+    ]
+  end
+
+  def job_seeker_contact_preferences
+    [
+      [I18n.t('contacts.whatsapp'), 'whatsapp' ],
+      [I18n.t('contacts.mobile'), 'mobile' ],
+      [I18n.t('contacts.email'), 'email' ],
+      [I18n.t('contacts.phone'), 'phone' ]
+    ]
+  end
 end
