6726b88fa2fe61236db25f601c1faf81c50c6541
Michael Kessler <michi@netzpiraten.ch>
2013-11-29 23:31:21 +0100

    Add postal to preferred contacts list for providers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
   def job_provider_contact_preferences
     [
+      [I18n.t('contacts.postal'), 'postal' ],
       [I18n.t('contacts.phone'), 'phone' ],
       [I18n.t('contacts.email'), 'email' ],
       [I18n.t('contacts.mobile'), 'mobile' ]
