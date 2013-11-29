0107c84f9ae08d0fb540e1ce37632ebd4835d468
Michael Kessler <michi@netzpiraten.ch>
2013-11-29 20:05:41 +0100

    Fix some mobile issues on the homepage.

--- config/navigation.rb
+++ config/navigation.rb
@@ -1,5 +1,5 @@
 SimpleNavigation::Configuration.run do |navigation|
-  navigation.renderer = SimpleNavigationRenderers::Bootstrap3
+  navigation.renderer = SimpleNavigationRenderers::Bootstrap2
   navigation.items do |primary|
     primary.item :home, { icon: 'fa fa-home fa-lg', text: I18n.t('navigation.home') }, root_path
     primary.dom_class = 'nav-pills pull-right'
