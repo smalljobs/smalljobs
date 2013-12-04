6d04b993920159ff7b55b25dfe21bfd716d4555c
Michael Kessler <michi@netzpiraten.ch>
2013-12-04 16:28:33 +0100

    Add specs for omniauth controller.

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -4,15 +4,15 @@ class OmniauthCallbacksController < ::Devise::OmniauthCallbacksController
     job_seeker = JobSeeker.find_for_facebook_oauth(request.env['omniauth.auth'], current_job_seeker)
 
     if job_seeker.persisted?
-      if job_seeker.confirmed? && job_seeker.activated?
+      if job_seeker.confirmed? && job_seeker.active?
         sign_in_and_redirect :job_seeker, job_seeker, event: :authentication
         set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
 
       elsif job_seeker.confirmed?
-        redirect_to activate_url
+        redirect_to awaiting_activation_url
 
       else
-        redirect_to confirm_url
+        redirect_to awaiting_confirmation_url
       end
     else
       session['devise.facebook_data'] = request.env['omniauth.auth']
---
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -0,0 +1,57 @@
+require 'spec_helper'
+
+describe OmniauthCallbacksController do
+
+  describe '#facebook' do
+    before do
+      request.env['devise.mapping'] = Devise.mappings[:job_seeker]
+    end
+
+    context 'with persisted user' do
+      context 'which is confirmed and activated' do
+        before do
+          JobSeeker.stub(:find_for_facebook_oauth).and_return(Fabricate(:job_seeker, confirmed: true, active: true))
+          get :facebook
+        end
+
+        it 'redirects the user to its dashboard' do
+          expect(response).to redirect_to('/job_seekers/dashboard')
+        end
+      end
+
+      context 'which is not confirmed' do
+        before do
+          JobSeeker.stub(:find_for_facebook_oauth).and_return(Fabricate(:job_seeker, confirmed: false, active: true))
+          get :facebook
+        end
+
+        it 'redirects the user to the awaiting confirmation page' do
+          expect(response).to redirect_to('/awaiting_confirmation')
+        end
+      end
+
+      context 'which is not activated' do
+        before do
+          JobSeeker.stub(:find_for_facebook_oauth).and_return(Fabricate(:job_seeker, confirmed: true, active: false))
+          get :facebook
+        end
+
+        it 'redirects the user to the awaiting activation page' do
+          expect(response).to redirect_to('/awaiting_activation')
+        end
+      end
+    end
+
+    context 'with a user that cannot be persisted' do
+      before do
+        JobSeeker.stub(:find_for_facebook_oauth).and_return(Fabricate.build(:job_seeker))
+        get :facebook
+      end
+
+      it 'redirects to the job seeker registration' do
+        expect(response).to redirect_to('/job_seekers/sign_up')
+      end
+    end
+  end
+
+end
