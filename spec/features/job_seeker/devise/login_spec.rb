4f0a57fc93de0d05c6a2a1cae99c406ea2f7983c
Michael Kessler <michi@netzpiraten.ch>
2013-12-18 14:54:40 +0100

    Login specs.

---
+++ spec/features/job_seeker/devise/login_spec.rb
@@ -0,0 +1,100 @@
+# coding: UTF-8
+
+require 'spec_helper'
+
+feature 'Login' do
+  context 'with an unconfirmed user' do
+    background do
+      Fabricate(:job_seeker,
+                email: 'rolf@example.com',
+                password: 'tester1234',
+                password_confirmation: 'tester1234',
+                confirmed: false,
+                active: false)
+    end
+
+    scenario 'using valid credentials' do
+      visit '/'
+      click_on 'Anmelden'
+      click_on 'Sucher'
+
+      fill_in 'Email',    with: 'rolf@example.com'
+      fill_in 'Passwort', with: 'tester1234'
+
+      click_on 'Als Jobsucher anmelden'
+
+      within_notifications do
+        expect(page).to have_content('Sie müssen Ihren Account bestätigen, bevor Sie fortfahren können.')
+      end
+    end
+  end
+
+  context 'with an inactive user' do
+    background do
+      Fabricate(:job_seeker,
+                email: 'rolf@example.com',
+                password: 'tester1234',
+                password_confirmation: 'tester1234',
+                confirmed: true,
+                active: false)
+    end
+
+    scenario 'using valid credentials' do
+      visit '/'
+      click_on 'Anmelden'
+      click_on 'Sucher'
+
+      fill_in 'Email',    with: 'rolf@example.com'
+      fill_in 'Passwort', with: 'tester1234'
+
+      click_on 'Als Jobsucher anmelden'
+
+      within_notifications do
+        expect(page).to have_content('Ihr Account ist nicht aktiv')
+      end
+    end
+  end
+
+  context 'with an active user' do
+    background do
+      Fabricate(:job_seeker,
+                email: 'rolf@example.com',
+                password: 'tester1234',
+                password_confirmation: 'tester1234',
+                confirmed: true,
+                active: true)
+    end
+
+    scenario 'using invalid credentials' do
+      visit '/'
+      click_on 'Anmelden'
+      click_on 'Sucher'
+
+      fill_in 'Email',    with: 'dani'
+      fill_in 'Passwort', with: 'tester'
+
+      click_on 'Als Jobsucher anmelden'
+
+      within_notifications do
+        expect(page).to have_content('Ungültige Anmeldedaten')
+      end
+    end
+
+    scenario 'using valid credentials' do
+      visit '/'
+      click_on 'Anmelden'
+      click_on 'Sucher'
+
+      fill_in 'Email',    with: 'rolf@example.com'
+      fill_in 'Passwort', with: 'tester1234'
+
+      click_on 'Als Jobsucher anmelden'
+
+      within_notifications do
+        expect(page).to have_content('Erfolgreich angemeldet')
+      end
+
+      expect(current_path).to eql('/job_seekers/dashboard')
+    end
+  end
+end
