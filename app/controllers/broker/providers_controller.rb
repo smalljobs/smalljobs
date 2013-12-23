c6cc4e33c7fbb895a1311c76ab8dc17ea3943ac8
Michael Kessler <michi@netzpiraten.ch>
2013-12-23 23:52:46 +0100

    Optional password when a broker edits a provider

--- app/controllers/broker/providers_controller.rb
+++ app/controllers/broker/providers_controller.rb
@@ -1,4 +1,21 @@
 class Broker::ProvidersController < InheritedResources::Base
+
   before_filter :authenticate_broker!
+  before_filter :optional_password, only: [:update]
+
   load_and_authorize_resource :provider, through: :current_broker
+
+  protected
+
+  def optional_password
+    if params[:provider][:password].blank?
+      params[:provider].delete(:password)
+      params[:provider].delete(:password_confirmation)
+    end
+  end
+
+  def permitted_params
+    params.permit(provider: [:id, :username, :password, :password_confirmation, :firstname, :lastname, :street, :zip, :city, :email, :phone, :mobile, :contact_preference, :contact_availability])
+  end
+
 end
--- spec/controllers/broker/providers_controller_spec.rb
+++ spec/controllers/broker/providers_controller_spec.rb
@@ -1,4 +1,69 @@
 require 'spec_helper'
 
 describe Broker::ProvidersController do
+  #it_should_behave_like 'a protected controller', :broker, :provider, :all
+
+  describe '#optional_password' do
+    let(:provider) { Fabricate(:provider, zip: '1234') }
+
+    before do
+      authenticate(:broker, Fabricate(:broker_with_regions))
+    end
+
+    context 'without a password' do
+      let(:attributes) do
+        attributes = provider.attributes
+        attributes['firstname'] = 'A test'
+        attributes.delete(:password)
+        attributes.delete(:password_confirmation)
+        attributes
+      end
+
+      it 'saves the update data' do
+        patch :update, id: provider.id, provider: attributes
+        provider.reload
+        expect(provider.firstname).to eql('A test')
+      end
+
+      it 'redirects to the updated provider' do
+        patch :update, id: provider.id, provider: attributes
+        expect(response).to redirect_to(broker_provider_path(provider))
+      end
+    end
+
+    context 'with a password' do
+      context 'that is not confirmed' do
+        let(:attributes) do
+          attributes = provider.attributes
+          attributes['firstname'] = 'A test'
+          attributes['password'] = 'tester1234'
+          attributes['password_confirmation'] = 'tester'
+          attributes
+        end
+
+        it 'does not save the updated data' do
+          patch :update, id: provider.id, provider: attributes
+          provider.reload
+          expect(provider.firstname).to_not eql('A test')
+        end
+      end
+
+      context 'that is confirmed' do
+        let(:attributes) do
+          attributes = provider.attributes
+          attributes['firstname'] = 'A test'
+          attributes['password'] = 'tester1234'
+          attributes['password_confirmation'] = 'tester1234'
+          attributes
+        end
+
+        it 'does save the updated data' do
+          patch :update, id: provider.id, provider: attributes
+          provider.reload
+          expect(provider.firstname).to eql('A test')
+          expect(provider.valid_password?('tester1234')).to be_true
+        end
+      end
+    end
+  end
 end
