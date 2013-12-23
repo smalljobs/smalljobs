d138e6fc1a855ede80311945f16499fc9671fba6
Michael Kessler <michi@netzpiraten.ch>
2013-12-23 11:43:20 +0100

    Fix seeker spec.

--- app/models/seeker.rb
+++ app/models/seeker.rb
@@ -106,7 +106,7 @@ class Seeker < ActiveRecord::Base
   # @return [Boolean] validation status
   #
   def ensure_seeker_age
-    if date_of_birth.present? && !date_of_birth.between?(18.years.ago, 12.years.ago)
+    if date_of_birth.present? && !date_of_birth.between?(19.years.ago, 13.years.ago)
       errors.add(:date_of_birth, :invalid_seeker_age)
     end
   end
