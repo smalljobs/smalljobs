c423e021340141bbe7f76eadc71f4be6537f1f20
Michael Kessler <michi@flinkfinger.com>
2014-01-21 10:11:00 +0100

    Fix spec name.

--- spec/routing/provider_spec.rb
+++ spec/routing/provider_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'routing to the provider dashboard' do
-  it 'routes /provider/dashboard to provider/dashboard#index' do
+  it 'routes /provider/dashboard to provider/dashboard#show' do
     expect(get: '/provider/dashboard').to route_to(
       controller: 'provider/dashboards',
       action:     'show'
--- spec/routing/seeker_spec.rb
+++ spec/routing/seeker_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'routing to the seeker dashboard' do
-  it 'routes /seeker/dashboard to seeker/dashboard#index' do
+  it 'routes /seeker/dashboard to seeker/dashboard#show' do
     expect(get: '/seeker/dashboard').to route_to(
       controller: 'seeker/dashboards',
       action:     'show'
