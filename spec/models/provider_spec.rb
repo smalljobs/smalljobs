00cc4a24cf778b25aac0a412eb473a656451ca45
Michael Kessler <michi@flinkfinger.com>
2014-01-22 15:31:54 +0100

    Allow brokers toggle providers active and confirmed state.

--- spec/models/provider_spec.rb
+++ spec/models/provider_spec.rb
@@ -116,6 +116,28 @@ describe Provider do
     end
   end
 
+  describe '#confirmed=' do
+    context 'when not confirmed' do
+      let(:provider) { Fabricate(:provider, confirmed: false) }
+
+      it 'confirms the user when set to 1' do
+        expect(provider.confirmed?).to be_false
+        provider.confirmed = 1
+        expect(provider.confirmed?).to be_true
+      end
+    end
+
+    context 'when already confirmed' do
+      let(:provider) { Fabricate(:provider, confirmed: true) }
+
+      it 'unconfirms the user when set to 0' do
+        expect(provider.confirmed?).to be_true
+        provider.confirmed = 0
+        expect(provider.confirmed?).to be_false
+      end
+    end
+  end
+
   describe '#email_required?' do
     it 'does not require an email' do
       expect(Fabricate(:provider).email_required?).to be_false
