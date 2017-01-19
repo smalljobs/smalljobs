dc5f4ad1ec9124e4e37bafc05ba82d007b062105
blackbat13 <blackbat13@gmail.com>
2017-01-19 17:22:46 +0100

    Cleared seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,27 +10,3 @@ CSV.foreach(File.expand_path(File.join('db', 'data', 'postal_codes', 'CH.csv')),
       latitude: row[10]
   ).first_or_create
 end
-
-Place.all.find_each do |place|
-  place.region_id = 0
-  place.save!
-end
-
-place = Place.first
-place.region_id = 1
-place.save!
-
-Region.all.find_each do |region|
-  region.destroy
-end
-Region.create!(subdomain: 'smalljobs', name: 'smalljobs', id: 1)
-
-Organization.where(
-    name: 'smalljobs org',
-    website: 'smalljobs.herokuapp.com',
-    description: 'smalljobs organization',
-    street: 'Street 1',
-    email: 'email@mail.com',
-    phone: '111111111',
-    place_id: place.id
-).first_or_create
