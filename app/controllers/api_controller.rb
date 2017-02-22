22adb2bc0a8a7600be516caabb8fea4010935743
blackbat13 <blackbat13@gmail.com>
2017-02-22 19:12:10 +0100

    Added categories to user

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -53,14 +53,12 @@ class ApiController < ApplicationController
     user_params.except!(:zip)
     user_params[:mobile] = user_params[:phone]
     user_params[:work_category_ids] = user_params[:categories]
-    user_params.except![:categories]
+    user_params.except!(:categories)
     user_params[:password_confirmation] = user_params[:password]
-    render json: {code: 'users/invalid',  user: user_params}, status: 422
-    return
     seeker = Seeker.new(user_params)
     seeker.status = 1
     if !seeker.save
-      render json: {code: 'users/invalid', message: seeker.errors.first, categories: user_params[:work_category_ids]}, status: 422
+      render json: {code: 'users/invalid', message: seeker.errors.first}, status: 422
       return
     end
 
