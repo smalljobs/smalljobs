b733c014413621f03c2492656e55f232b2e074d9
blackbat13 <blackbat13@gmail.com>
2017-02-27 19:40:43 +0100

    Modified job edit view for provider

--- app/controllers/provider/jobs_controller.rb
+++ app/controllers/provider/jobs_controller.rb
@@ -5,9 +5,14 @@ class Provider::JobsController < InheritedResources::Base
   load_and_authorize_resource :job, through: :current_provider, parent: false
   skip_authorize_resource :job, only: [:new, :create]
 
+  def new
+    @job = Job.new(state: 'hidden')
+  end
+
   def create
     @job = Job.new(permitted_params[:job])
     @job.provider = current_provider
+    @job.state = 'hidden'
 
     authorize!(:create, @job)
     create!
