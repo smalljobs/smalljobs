4decdebe6ae965020e1d7b0f38fcfb5573d3d8f1
blackbat13 <blackbat13@gmail.com>
2017-04-04 14:20:36 +0200

    Added assignment default start time

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -7,4 +7,13 @@ class Assignment < ActiveRecord::Base
   validates :seeker, presence: true
 
   enum status: [:active, :finished]
+
+  before_save :set_start
+
+  # Sets start time if not provided
+  def set_start
+    if self.start_time == nil
+      self.start_time = DateTime.now()
+    end
+  end
 end
