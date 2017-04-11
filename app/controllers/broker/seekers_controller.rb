fab3f64869f471e91aee2223e0545b33828bac4e
blackbat13 <blackbat13@gmail.com>
2017-04-11 10:25:12 +0200

    Changed live to dev

--- app/controllers/broker/seekers_controller.rb
+++ app/controllers/broker/seekers_controller.rb
@@ -28,13 +28,13 @@ class Broker::SeekersController < InheritedResources::Base
   def edit
     dev = "https://devadmin.jugendarbeit.digital/api/jugendinfo_message/get_conversation_id_by_user?user_id=#{@seeker.app_user_id}"
     live = "https://admin.jugendarbeit.digital/api/jugendinfo_message/get_conversation_id_by_user?user_id=#{@seeker.app_user_id}"
-    response = RestClient.get live
+    response = RestClient.get dev
     json = JSON.parse(response)
     conversation_id = json['id']
     if conversation_id != nil
       dev = "https://devadmin.jugendarbeit.digital/api/jugendinfo_message/get_messages/?key=ULv8r9J7Hqc7n2B8qYmfQewzerhV9p&id=#{conversation_id}&limit=1000"
       live = "https://admin.jugendarbeit.digital/api/jugendinfo_message/get_messages/?key=ULv8r9J7Hqc7n2B8qYmfQewzerhV9p&id=#{conversation_id}&limit=1000"
-      response = RestClient.get live
+      response = RestClient.get dev
       json = JSON.parse(response.body)
       @messages = json['messages'].sort_by {|val| DateTime.strptime(val['datetime'], '%s')}.reverse
     else
@@ -76,7 +76,7 @@ class Broker::SeekersController < InheritedResources::Base
     require 'rest-client'
     dev = "https://devadmin.jugendarbeit.digital/api/jugendinfo_push/send"
     live = "https://admin.jugendarbeit.digital/api/jugendinfo_push/send"
-    response = RestClient.post live, {api: 'ULv8r9J7Hqc7n2B8qYmfQewzerhV9p', message_title: title, message: message, device_token: @seeker.app_user_id, sendermail: current_broker.email}
+    response = RestClient.post dev, {api: 'ULv8r9J7Hqc7n2B8qYmfQewzerhV9p', message_title: title, message: message, device_token: @seeker.app_user_id, sendermail: current_broker.email}
     json = JSON.parse(response.body)
     conv_id = json['conversation_id']
 
