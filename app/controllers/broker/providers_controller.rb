419b2ccb939db3c098540816a2dd0f6c9a13ce30
blackbat13 <blackbat13@gmail.com>
2017-04-16 02:18:28 +0200

    Fixed saving contract

--- app/controllers/broker/providers_controller.rb
+++ app/controllers/broker/providers_controller.rb
@@ -74,7 +74,7 @@ class Broker::ProvidersController < InheritedResources::Base
   end
 
   def permitted_params
-    params.permit(provider: [:id, :username, :password, :password_confirmation, :firstname, :lastname, :street, :place_id, :email, :phone, :mobile, :contact_preference, :contact_availability, :active, :confirmed, :organization_id, :notes, :company, :state])
+    params.permit(provider: [:id, :username, :password, :password_confirmation, :firstname, :lastname, :street, :place_id, :email, :phone, :mobile, :contact_preference, :contact_availability, :active, :confirmed, :organization_id, :notes, :company, :state, :contract])
   end
 
 end
