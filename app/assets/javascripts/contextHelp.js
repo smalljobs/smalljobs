e1bbda905f023f9a502a6e449074fa35872cea26
blackbat13 <blackbat13@gmail.com>
2017-05-02 19:51:36 +0200

    Fix for context help

--- app/assets/javascripts/contextHelp.js
+++ app/assets/javascripts/contextHelp.js
@@ -3,7 +3,7 @@ function setHelp() {
         var $helpModalContent = $('#helpModalContent');
         var $helpModalTitle = $('#helpModalTitle');
         var $helpButton = $('#helpButton');
-        if(response['description'] !== null) {
+        if(response['description'] !== null && response['description'] !== undefined) {
             $helpModalContent.html(response['description']);
             $helpModalTitle.html(response['title']);
             $helpButton.removeClass('display-none');
