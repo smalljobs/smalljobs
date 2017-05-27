da44a99e21e00f93264b391d815ca06a42fa12ed
blackbat13 <blackbat13@gmail.com>
2017-05-27 10:09:42 +0200

    Modified aws config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,6 +94,7 @@ Smalljobs::Application.configure do
   config.paperclip_defaults = {
       :storage => :s3,
       :s3_region => ENV['AWS_REGION'],
+      :s3_host_name => 's3.eu-west-2.amazonaws.com',
       :s3_credentials => {
           :bucket => ENV['S3_BUCKET_NAME'],
           :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
