1f250ba5d12e45864567d31719e3da1f2c8ec235
blackbat13 <blackbat13@gmail.com>
2017-05-29 12:56:01 +0200

    Changed order of default sorting in broker dashboard

--- app/controllers/broker/dashboards_controller.rb
+++ app/controllers/broker/dashboards_controller.rb
@@ -7,7 +7,7 @@ class Broker::DashboardsController < ApplicationController
   load_and_authorize_resource :seeker, through: :current_region
 
   def show
-    @jobs = current_broker.jobs.includes(:provider, :organization).order(:last_change_of_state)
+    @jobs = current_broker.jobs.includes(:provider, :organization).order(:last_change_of_state).reverse_order()
     allocations = Allocation.where(job: @jobs).includes(:seeker)
     @allocations = []
     allocations.each do |allocation|
@@ -16,10 +16,10 @@ class Broker::DashboardsController < ApplicationController
       @allocations[allocation.job_id][Allocation.states[allocation.state]].push(allocation)
     end
 
-    @providers = current_broker.providers.includes(:place, :jobs, :organization).order(:updated_at)
-    @seekers = current_broker.seekers.includes(:place, :organization).order(:updated_at)
-    @assignments = current_broker.assignments.includes(:seeker, :provider).order(:created_at)
-    @todos = Todo.where(seeker: @seekers).or(Todo.where(provider: @providers)).or(Todo.where(job: @jobs)).or(Todo.where(allocation: allocations)).order(:created_at)
+    @providers = current_broker.providers.includes(:place, :jobs, :organization).order(:updated_at).reverse_order()
+    @seekers = current_broker.seekers.includes(:place, :organization).order(:updated_at).reverse_order()
+    @assignments = current_broker.assignments.includes(:seeker, :provider).order(:created_at).reverse_order()
+    @todos = Todo.where(seeker: @seekers).or(Todo.where(provider: @providers)).or(Todo.where(job: @jobs)).or(Todo.where(allocation: allocations)).order(:created_at).reverse_order()
   end
 
   def save_settings
