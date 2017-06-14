8e69e8bd58ba3299fe7062d53a6352707a1cc8d8
blackbat13 <blackbat13@gmail.com>
2017-06-14 11:33:42 +0200

    Modified migration

--- db/migrate/20170614083159_add_provider_ref_to_allocations.rb
+++ db/migrate/20170614083159_add_provider_ref_to_allocations.rb
@@ -6,7 +6,7 @@ class AddProviderRefToAllocations < ActiveRecord::Migration[5.0]
     add_reference :allocations, :provider, index: true
 
     Allocation.all.each do |allocation|
-      allocation.update_attributes!(provider_id: allocation.job.provider_id)
+      allocation.update_attributes!(provider_id: allocation.job.provider_id) unless allocation.job.nil?
     end
   end
 end
