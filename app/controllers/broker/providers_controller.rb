08afbc5ad8496f7725a7009a289ad11c97d51c50
blackbat13 <blackbat13@gmail.com>
2017-06-28 13:00:44 +0200

    Modified letter spacing

--- app/controllers/broker/providers_controller.rb
+++ app/controllers/broker/providers_controller.rb
@@ -34,7 +34,7 @@ class Broker::ProvidersController < InheritedResources::Base
     @qrcode = RQRCode::QRCode.new(@provider.id.to_s, mode: :number).as_png
 
     if request.subdomain == 'winterthur' || params['domain'] == 'winterthur'
-      render pdf: 'contract', template: 'broker/providers/contract_winterthur.html.erb'
+      render pdf: 'contract', template: 'broker/providers/contract_winterthur.html.erb', dpi: '96'
     else
       render pdf: 'contract', template: 'broker/providers/contract.html.erb'
     end
