ff8c5e8bb4403a0720b0491a3bce7ffc70296751
blackbat13 <blackbat13@gmail.com>
2017-07-11 12:36:33 +0200

    Modified password recovery

---
+++ app/controllers/password_controller.rb
@@ -0,0 +1,17 @@
+class PasswordsController < Devise::PasswordsController
+  def create
+    if !Broker.find_by(email: resource_params[:email]).nil?
+      self.resource = Broker.send_reset_password_instructions(resource_params)
+    else
+      self.resource = resource_class.send_reset_password_instructions(resource_params)
+    end
+
+    yield resource if block_given?
+
+    if successfully_sent?(resource)
+      respond_with({}, location: after_sending_reset_password_instructions_path_for(resource_name))
+    else
+      respond_with(resource)
+    end
+  end
+end
