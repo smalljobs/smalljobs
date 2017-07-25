21e5d54fe67582a826de5859d3ff5d9eab42cf80
blackbat13 <blackbat13@gmail.com>
2017-07-25 19:03:51 +0200

    Changed dev to live

--- app/models/seeker.rb
+++ app/models/seeker.rb
@@ -151,7 +151,7 @@ class Seeker < ActiveRecord::Base
     dev = 'https://devadmin.jugendarbeit.digital/api/jugendinfo_user/update_data/'
     live = 'https://admin.jugendarbeit.digital/api/jugendinfo_user/update_data/'
     begin
-      RestClient.post dev, {token: '1bN1SO2W1Ilz4xL2ld364qVibI0PsfEYcKZRH', id: app_user_id, smalljobs_user_id: id, firstname: firstname, lastname: lastname, mobile: mobile, address: street, zip: place.zip, birthdate: date_of_birth.strftime('%Y-%m-%d'), city: place.name, smalljobs_status: Seeker.statuses[status]}
+      RestClient.post live, {token: '1bN1SO2W1Ilz4xL2ld364qVibI0PsfEYcKZRH', id: app_user_id, smalljobs_user_id: id, firstname: firstname, lastname: lastname, mobile: mobile, address: street, zip: place.zip, birthdate: date_of_birth.strftime('%Y-%m-%d'), city: place.name, smalljobs_status: Seeker.statuses[status]}
     rescue
       nil
     end
