a61b6d185c70ea114cdc3360fdc8d01d39d428e6
blackbat13 <blackbat13@gmail.com>
2017-08-30 09:57:44 +0200

    Small change to provider contract

--- app/controllers/broker/providers_controller.rb
+++ app/controllers/broker/providers_controller.rb
@@ -44,11 +44,7 @@ class Broker::ProvidersController < InheritedResources::Base
     require 'rqrcode'
     @qrcode = RQRCode::QRCode.new(@provider.id.to_s, mode: :number).as_png
 
-    if request.subdomain == 'winterthur' || params['domain'] == 'winterthur'
-      render pdf: 'contract', template: 'broker/providers/contract_winterthur.html.erb', dpi: '96'
-    else
-      render pdf: 'contract', template: 'broker/providers/contract.html.erb'
-    end
+    render pdf: 'contract', template: 'broker/providers/contract.html.erb'
   end
 
   def delete
