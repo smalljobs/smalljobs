05227e8b9fa6deffacdb7e191ead6164db70b6ba
blackbat <blackbat13@gmail.com>
2017-10-31 20:27:39 +0100

    Added loading images in agreement

--- app/uploaders/background_uploader.rb
+++ app/uploaders/background_uploader.rb
@@ -4,7 +4,7 @@ class BackgroundUploader < CarrierWave::Uploader::Base
   storage :fog
 
   def store_dir
-    "uploads-dev/#{ model.class.to_s.underscore }/#{ mounted_as }/#{ model.id }"
+    "uploads/#{ model.class.to_s.underscore }/#{ mounted_as }/#{ model.id }"
   end
 
   def extension_white_list
