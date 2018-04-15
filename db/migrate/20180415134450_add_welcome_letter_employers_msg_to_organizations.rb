ae03d2ca489700aa77ab6e97ef5ac5fd3809bf4c
blackbat13 <blackbat13@gmail.com>
2018-04-15 14:12:22 +0200

    Modified migration default value

--- db/migrate/20180415134450_add_welcome_letter_employers_msg_to_organizations.rb
+++ db/migrate/20180415134450_add_welcome_letter_employers_msg_to_organizations.rb
@@ -6,7 +6,7 @@ class AddWelcomeLetterEmployersMsgToOrganizations < ActiveRecord::Migration[5.0]
     add_column :organizations, :welcome_letter_employers_msg, :text
 
     Organization.all.each do |organization|
-      organization.update_attributes!(welcome_letter_employers_msg: 'Guten Tag {{provider_first_name}} {{provider_last_name}}\nWillkommen bei Smalljobs.')
+      organization.update_attributes!(welcome_letter_employers_msg: "Guten Tag {{provider_first_name}} {{provider_last_name}}\nWillkommen bei Smalljobs.")
     end
   end
 end
