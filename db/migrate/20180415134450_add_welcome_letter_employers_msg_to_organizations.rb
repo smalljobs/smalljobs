6e97f9d040e8a1931433c3ab27263069b2484f1e
blackbat13 <blackbat13@gmail.com>
2018-04-15 13:52:17 +0200

    Added welcome letter employers message template to organizations

---
+++ db/migrate/20180415134450_add_welcome_letter_employers_msg_to_organizations.rb
@@ -0,0 +1,12 @@
+class AddWelcomeLetterEmployersMsgToOrganizations < ActiveRecord::Migration[5.0]
+  class Organizations < ActiveRecord::Base
+  end
+
+  def change
+    add_column :organizations, :welcome_letter_employers_msg, :text
+
+    Organization.all.each do |organization|
+      organization.update_attributes!(welcome_letter_employers_msg: 'Guten Tag {{provider_first_name}} {{provider_last_name}}\nWillkommen bei Smalljobs.')
+    end
+  end
+end
