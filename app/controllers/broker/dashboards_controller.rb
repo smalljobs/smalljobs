10aafbadb37a06a53c2f4087d16f57bea29e722b
blackbat13 <blackbat13@gmail.com>
2018-04-23 21:22:26 +0200

    Modified broker dashboard

--- app/controllers/broker/dashboards_controller.rb
+++ app/controllers/broker/dashboards_controller.rb
@@ -10,6 +10,7 @@ class Broker::DashboardsController < ApplicationController
     @hello = params[:archive]
     @hello2 = params['archive']
     @hello3 = params
+    @hello4 = permitted_params
     if params[:archive] == true || params[:archive] == 'true'
       @jobs = current_broker.jobs.where(state: 'finished').includes(:provider, :organization).group('jobs.id').order(:last_change_of_state).reverse_order()
       allocations = Allocation.where(job: @jobs).includes(:seeker)
