bb217d6321858fd909759599951ea9e98fa48ac0
blackbat13 <blackbat13@gmail.com>
2018-04-24 21:27:52 +0200

    Modified mustache renderers

--- app/controllers/broker/providers_controller.rb
+++ app/controllers/broker/providers_controller.rb
@@ -44,7 +44,8 @@ class Broker::ProvidersController < InheritedResources::Base
     require 'rqrcode'
     @broker = current_broker
     @qrcode = RQRCode::QRCode.new(@provider.id.to_s, mode: :number).as_png
-    @letter_msg = Mustache.render(@provider.organization.welcome_letter_employers_msg, provider_first_name: @provider.firstname, provider_last_name: @provider.lastname)
+    provider_phone = @provider.mobile.empty? ? @provider.phone : @provider.mobile
+    @letter_msg = Mustache.render(@provider.organization.welcome_letter_employers_msg, provider_first_name: @provider.firstname, provider_last_name: @provider.lastname, provider_phone: provider_phone, broker_first_name: current_broker.firstname, broker_last_name: current_broker.lastname, organization_name: @provider.organization.name, organization_zip: @provider.organization.place.zip, organization_street: @provider.organization.street, organization_place: @provider.organization.places.first.name, link_to_jobboard_list: url_for(root_url()))
     @letter_msg.gsub! "\n", "<br>"
     render pdf: 'contract', template: 'broker/providers/contract.html.erb', margin: {top: 0, left: 0, right: 0, bottom: 0}
   end
--- app/controllers/provider/dashboards_controller.rb
+++ app/controllers/provider/dashboards_controller.rb
@@ -12,7 +12,8 @@ class Provider::DashboardsController < ApplicationController
     @provider = current_provider
     @broker = @provider.organization.brokers.first
     @qrcode = RQRCode::QRCode.new(@provider.id.to_s, mode: :number).as_png
-    @letter_msg = Mustache.render(@provider.organization.welcome_letter_employers_msg, provider_first_name: @provider.firstname, provider_last_name: @provider.lastname)
+    provider_phone = @provider.mobile.empty? ? @provider.phone : @provider.mobile
+    @letter_msg = Mustache.render(@provider.organization.welcome_letter_employers_msg, provider_first_name: @provider.firstname, provider_last_name: @provider.lastname, provider_phone: provider_phone, broker_first_name: @broker.firstname, broker_last_name: @broker.lastname, organization_name: @provider.organization.name, organization_zip: @provider.organization.place.zip, organization_street: @provider.organization.street, organization_place: @provider.organization.places.first.name, link_to_jobboard_list: url_for(root_url()))
     @letter_msg.gsub! "\n", "<br>"
     render pdf: 'contract', template: 'broker/providers/contract.html.erb'
   end
