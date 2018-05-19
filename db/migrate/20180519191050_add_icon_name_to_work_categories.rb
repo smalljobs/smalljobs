535282957c423f5026b925282aceb3297f36a786
blackbat13 <blackbat13@gmail.com>
2018-05-19 19:16:56 +0200

    Added icon names to work categories

--- db/migrate/20180519191050_add_icon_name_to_work_categories.rb
+++ db/migrate/20180519191050_add_icon_name_to_work_categories.rb
@@ -1,4 +1,4 @@
-class AddLogoToRegions < ActiveRecord::Migration[5.0]
+class AddIconNameToWorkCategories < ActiveRecord::Migration[5.0]
   def change
     add_column :work_categories, :icon_name, :string
   end
