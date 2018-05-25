df928993c96e1131b674fd0e05a557bfdd2b65b9
blackbat13 <blackbat13@gmail.com>
2018-05-25 18:15:20 +0200

    Modified parent message

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -107,6 +107,7 @@ class ApiController < ApplicationController
 
     unless parents_email.nil?
       parent_message = Mustache.render(seeker.organization.welcome_email_for_parents_msg, seeker_first_name: seeker.firstname, seeker_last_name: seeker.lastname, seeker_link_to_agreement: url_for(agreement_broker_seeker_url(seeker, subdomain: 'winterthur')), broker_first_name: seeker.organization.brokers.first.firstname, broker_last_name: seeker.organization.brokers.first.lastname, organization_name: seeker.organization.name, organization_street: seeker.organization.street, organization_zip: seeker.organization.places.first.zip, organization_place: seeker.organization.places.first.name, organization_phone: seeker.organization.phone, organization_email: seeker.organization.email, link_to_jobboard_list: url_for(root_url(subdomain: 'winterthur')))
+      parent_message.gsub! "\n", "<br>"
       Notifier.send_welcome_message_for_parents(parent_message, parents_email).deliver
     end
 
