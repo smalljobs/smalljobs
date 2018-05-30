fb7972b22e3b1df5d9997e0f597d6d24340fc291
blackbat13 <blackbat13@gmail.com>
2018-05-30 17:48:38 +0200

    Modified send activation message

--- app/controllers/broker/providers_controller.rb
+++ app/controllers/broker/providers_controller.rb
@@ -45,7 +45,7 @@ class Broker::ProvidersController < InheritedResources::Base
     @broker = current_broker
     @qrcode = RQRCode::QRCode.new(@provider.id.to_s, mode: :number).as_png
     provider_phone = @provider.mobile.empty? ? @provider.phone : @provider.mobile
-    @letter_msg = Mustache.render(@provider.organization.welcome_letter_employers_msg, provider_first_name: @provider.firstname, provider_last_name: @provider.lastname, provider_phone: provider_phone, broker_first_name: current_broker.firstname, broker_last_name: current_broker.lastname, organization_name: @provider.organization.name, organization_zip: @provider.organization.place.zip, organization_street: @provider.organization.street, organization_place: @provider.organization.places.first.name, organization_phone: @provider.organization.phone, organization_email: @provider.organization.email, link_to_jobboard_list: url_for(root_url()))
+    @letter_msg = Mustache.render(@provider.organization.welcome_letter_employers_msg || '', provider_first_name: @provider.firstname, provider_last_name: @provider.lastname, provider_phone: provider_phone, broker_first_name: current_broker.firstname, broker_last_name: current_broker.lastname, organization_name: @provider.organization.name, organization_zip: @provider.organization.place.zip, organization_street: @provider.organization.street, organization_place: @provider.organization.places.first.name, organization_phone: @provider.organization.phone, organization_email: @provider.organization.email, link_to_jobboard_list: url_for(root_url()))
     @letter_msg.gsub! "\n", "<br>"
     render pdf: 'contract', template: 'broker/providers/contract.html.erb', margin: {top: 0, left: 0, right: 0, bottom: 0}
   end
