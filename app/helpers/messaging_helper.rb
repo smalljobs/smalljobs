ae27c53e64cc6c2463d5c632dfe5f6dc4be60dc1
blackbat13 <blackbat13@gmail.com>
2018-08-16 04:29:43 +0200

    Changed messages sending to jugendinfo

--- app/helpers/messaging_helper.rb
+++ app/helpers/messaging_helper.rb
@@ -13,7 +13,7 @@ module MessagingHelper
   #
   # @return [Json] response from the jugendinfo server
   def self.send_message(title, message, device_token, sendermail)
-    url = "#{@@current_url}/jugendinfo_message/send"
+    url = "#{@@current_url}/jugendinfo_push/send"
     response = RestClient.post url, api: 'ULv8r9J7Hqc7n2B8qYmfQewzerhV9p', message_title: title, message: message, device_token: device_token, sendermail: sendermail
     response
   end
