39b287e439c8731c74741ec88b80068a1a36e514
blackbat13 <blackbat13@gmail.com>
2018-11-08 18:21:57 +0100

    Added more logs for messaging

--- app/helpers/messaging_helper.rb
+++ app/helpers/messaging_helper.rb
@@ -15,6 +15,7 @@ module MessagingHelper
   def self.send_message(title, message, device_token, sendermail)
     url = "#{@@current_url}/jugendinfo_push/send"
     response = RestClient.post url, api: 'ULv8r9J7Hqc7n2B8qYmfQewzerhV9p', message_title: title, message: message, device_token: device_token, sendermail: sendermail
+    Rails.logger.info "send_message post #{url}: {api: #{api}, message_title: #{title}, message: #{message}, device_token: #{device_token}, sendermail: #{sendermail}}"
     Rails.logger.info "send_message response: #{response}"
     response
   end
@@ -28,6 +29,7 @@ module MessagingHelper
     url = "#{@@current_url}/jugendinfo_message/get_conversation_id_by_user?user_id=#{device_token}"
     begin
       response = RestClient.get url
+      Rails.logger.info "get_conversation_id get: #{url}"
       Rails.logger.info "get_conversation_id response: #{response}"
       json = JSON.parse(response)
       conversation_id = json['id']
@@ -48,6 +50,7 @@ module MessagingHelper
     unless conversation_id.nil?
       url = "#{@@current_url}/jugendinfo_message/get_messages/?key=ULv8r9J7Hqc7n2B8qYmfQewzerhV9p&id=#{conversation_id}&limit=1000"
       response = RestClient.get url
+      Rails.logger.info "get_conversation_id get: #{url}"
       Rails.logger.info "get_messages response: #{response}"
       json = JSON.parse(response.body)
       messages = json['messages'].sort_by {|val| DateTime.strptime(val['datetime'], '%s')}.reverse
@@ -67,6 +70,7 @@ module MessagingHelper
     unless conversation_id.nil?
       url = "#{@@current_url}/jugendinfo_message/get_messages/?key=ULv8r9J7Hqc7n2B8qYmfQewzerhV9p&id=#{conversation_id}&last=1"
       response = RestClient.get url
+      Rails.logger.info "get_conversation_id get: #{url}"
       Rails.logger.info "get_last_message response: #{response}"
       json = JSON.parse(response.body)
       messages = json['messages']
@@ -82,6 +86,7 @@ module MessagingHelper
     unless conversation_id.nil?
       url = "#{@@current_url}/jugendinfo_message/get_messages_count_from_conversation/?conversation_id=#{conversation_id}"
       response = RestClient.get url
+      Rails.logger.info "get_conversation_id get: #{url}"
       Rails.logger.info "get_messages_count response: #{response}"
       begin
         json = JSON.parse(response.body)
