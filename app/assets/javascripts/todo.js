6e22058476531426f21dfbd191a6d5f684b38de8
Łukasz Kucharski <lkucharski@netkodo.com>
2019-07-19 13:49:01 +0000

    number of records for each table

--- app/assets/javascripts/todo.js
+++ app/assets/javascripts/todo.js
@@ -12,11 +12,15 @@ $(document).ready(function() {
           tr_last.find('span').data('todo-type', 'current')
           tr_last.find('td').last().remove()
           $('.js-todo-current').find('tbody').prepend(tr_last)
+          $('.js-todo-tabs > li > a[href="#todo_current"] > .sj-text-red').html($('#todo_current').find('tbody').find('tr').length)
+          $('.js-todo-tabs > li > a[href="#todo_postponed"] > .sj-text-red').html($('#todo_postponed').find('tbody').find('tr').length-1)
         } else if(todo_type == 'current'){
           tr_last = tr.clone()
           tr_last.find('span').data('todo-type', 'postponed')
-          tr_last.append("<td>"+result.postponed+"</td>")
+          tr_last.append("<td>"+result.postponed+"</td>śś")
           $('.js-todo-postponed').find('tbody').prepend(tr_last)
+          $('.js-todo-tabs > li > a[href="#todo_postponed"] > .sj-text-red').html($('#todo_postponed').find('tbody').find('tr').length)
+          $('.js-todo-tabs > li > a[href="#todo_current"] > .sj-text-red').html($('#todo_current').find('tbody').find('tr').length-1)
         }
         $('.js-postpone-msg').text(result.message)
         $('.js-postpone-msg').show();
