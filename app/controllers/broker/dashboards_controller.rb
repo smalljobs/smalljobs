b40d3ba2a17c637ef4b110f0aaa6f5b0259631a4
Łukasz Kucharski <lkucharski@netkodo.com>
2019-07-23 10:08:10 +0000

    ram usage

--- app/controllers/broker/dashboards_controller.rb
+++ app/controllers/broker/dashboards_controller.rb
@@ -10,7 +10,7 @@ class Broker::DashboardsController < ApplicationController
     if params[:archive] == true || params[:archive] == 'true'
       @jobs = current_broker.jobs.where(state: 'finished').includes(:provider, :organization).group('jobs.id').order(:last_change_of_state).reverse_order()
       allocations = Allocation.where(job: @jobs).includes(:seeker)
-      @allocations = []
+      @allocations = {}
       allocations.each do |allocation|
         @allocations[allocation.job_id] = [] if @allocations[allocation.job_id].nil?
         @allocations[allocation.job_id][Allocation.states[allocation.state]] = [] if @allocations[allocation.job_id][Allocation.states[allocation.state]].nil?
@@ -20,13 +20,22 @@ class Broker::DashboardsController < ApplicationController
       @providers = current_broker.providers.where(state: 3).includes(:place, :jobs, :organization).group('providers.id').order(:updated_at).reverse_order()
       @seekers = current_broker.seekers.where(status: 3).includes(:place, :organization).group('seekers.id').order(:updated_at).reverse_order()
       @assignments = current_broker.assignments.where(job: @jobs).includes(:seeker, :provider).group('assignments.id').order(:created_at).reverse_order()
-      @todos = Todo.where(seeker: @seekers).or(Todo.where(provider: @providers)).or(Todo.where(job: @jobs)).or(Todo.where(allocation: allocations)).group('todos.id').order(:created_at).reverse_order()
+      @todos = Todo.includes(:seeker, :provider, :job, :allocation, :todotype)
+                 .includes(
+                   seeker: :organization,
+                   provider: :organization,
+                   job: :organization,
+                   allocation: :organization,
+                   )
+                 .where("seeker_id IN (?) OR provider_id IN (?) OR job_id IN (?) OR allocation_id IN (?)",
+                        @seekers.pluck(:id), @providers.pluck(:id), @jobs.pluck(:id), allocations.pluck(:id))
+                 .reverse_order()
       return
     end
 
     @jobs = current_broker.jobs.where.not(state: 'finished').includes(:provider, :organization).group('jobs.id').order(:last_change_of_state).reverse_order()
     allocations = Allocation.where(job: @jobs).includes(:seeker)
-    @allocations = []
+    @allocations = {}
     allocations.each do |allocation|
       @allocations[allocation.job_id] = [] if @allocations[allocation.job_id].nil?
       @allocations[allocation.job_id][Allocation.states[allocation.state]] = [] if @allocations[allocation.job_id][Allocation.states[allocation.state]].nil?
@@ -36,7 +45,16 @@ class Broker::DashboardsController < ApplicationController
     @providers = current_broker.providers.where.not(state: 3).includes(:place, :jobs, :organization).group('providers.id').order(:updated_at).reverse_order()
     @seekers = current_broker.seekers.where.not(status: 3).includes(:place, :organization).group('seekers.id').order(:updated_at).reverse_order()
     @assignments = current_broker.assignments.where(job: @jobs).includes(:seeker, :provider).group('assignments.id').order(:created_at).reverse_order()
-    @todos = Todo.where(seeker: @seekers).or(Todo.where(provider: @providers)).or(Todo.where(job: @jobs)).or(Todo.where(allocation: allocations)).group('todos.id').order(:created_at).reverse_order()
+    @todos = Todo.includes(:seeker, :provider, :job, :allocation, :todotype)
+               .includes(
+                 seeker: :organization,
+                 provider: :organization,
+                 job: :organization,
+                 allocation: :organization,
+                 )
+               .where("seeker_id IN (?) OR provider_id IN (?) OR job_id IN (?) OR allocation_id IN (?)",
+                      @seekers.pluck(:id), @providers.pluck(:id), @jobs.pluck(:id), allocations.pluck(:id))
+               .reverse_order()
   end
 
   # Save broker settings from dashboard (current filter and selected organization)
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -43,10 +43,13 @@ class Organization < ActiveRecord::Base
   def copy_default_templates
     template_names = ['welcome_letter_employers_msg', 'welcome_app_register_msg', 'welcome_chat_register_msg',
                       'not_receive_job_msg', 'get_job_msg', 'activation_msg', 'welcome_email_for_parents_msg']
-    for template_name in template_names
-      default_template = DefaultTemplate.find_by(template_name: template_name)
-      next if default_template.nil?
-      self[template_name] = default_template.template if self[template_name].blank?
+    to_download = []
+    template_names.each{ |template_name| to_download << template_name if self[template_name].blank? }
+    if to_download.present?
+      DefaultTemplate.where(template_name: to_download).each do |default_template|
+        template_name = default_template.template_name
+        self[template_name] = default_template.template if self[template_name].blank?
+      end
     end
   end
 
