3ad110a4409487fa933451301c84b0ac0aeab480
Martin Nasierowski <mnasierowski@netkodo.com>
2019-07-29 11:55:38 +0000

    add spinner when data is loading

--- app/assets/javascripts/todo.js
+++ app/assets/javascripts/todo.js
@@ -62,7 +62,7 @@ $(document).ready(function() {
       which_todo = $('.js-todo-tabs').find('.active > a').attr('href')
       if($('.js-active-tab').find('.active > a').attr('href') == '#todos') {
         $(which_todo).show()
-        console.log(which_todo)
+        // console.log(which_todo)
         if(which_todo == '#todo_current'){
           $('#todo_postponed').hide()
         }else{
