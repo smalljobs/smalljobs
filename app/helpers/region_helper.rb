2b238231db4dd9e6fc0c31d15b87997f99362ef3
Przemysław Zając <zajac@netkodo.com>
2020-08-14 23:28:33 +0200

    Fixed style in helper

--- app/helpers/region_helper.rb
+++ app/helpers/region_helper.rb
@@ -1,7 +1,12 @@
 module RegionHelper
   def header_image_for_region(region)
     if region.present? and region.header_image.present?
-      return "background: linear-gradient(180deg, rgba(6,47,39,0.9) 0%, rgba(6,47,39,0) 50%, rgba(6,47,39,0) 100%), url(#{region.header_image.url}) no-repeat top center fixed;"
+      return "background: linear-gradient(180deg, rgba(6,47,39,0.9) 0%, rgba(6,47,39,0) 50%, rgba(6,47,39,0) 100%),"+
+          "url(#{region.header_image.url}) no-repeat top center fixed;"+
+          "-webkit-background-size: contain;"+
+          "-moz-background-size: contain;"+
+          "-o-background-size: contain;"+
+          "background-size: contain;"
     end
   end
 end
