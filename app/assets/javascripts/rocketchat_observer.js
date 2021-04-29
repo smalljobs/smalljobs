600be23c03d391c48b1e7473cbf204d2ac34a985
Przemysław Zając <zajac@netkodo.com>
2021-04-29 23:33:07 +0200

    Fix comple ugly

--- app/assets/javascripts/rocketchat_observer.js
+++ app/assets/javascripts/rocketchat_observer.js
@@ -1,16 +1,21 @@
-const rocketChatIframe = { rcAvailableInIframe: false, iframeLoad: false, url: '' }
+"use strict";
+var rocketChatIframe = {
+	rcAvailableInIframe: false,
+	iframeLoad: false,
+	url: ''
+};
+var handler = function () {
+	return {
+		set: function (target, key, value) {
+			target[key] = value;
+			if (target.rcAvailableInIframe && target.iframeLoad && window.iframeEl.src.length !== target.url.length) {
+				console.log(target);
+				window.iframeEl.src = target.url;
+				document.querySelector('.js-rocketchat-icon').classList.remove('display-none');
+			}
+			return true;
+		}
+	};
+};
+var rocketChatProxy = new Proxy(rocketChatIframe, handler());
 
-const handler = () => {
-  return {
-    set: function (target, key, value) {
-      target[key] = value;
-      if (target.rcAvailableInIframe && target.iframeLoad && window.iframeEl.src.length !== target.url.length) {
-        console.log(target)
-        window.iframeEl.src = target.url
-        document.querySelector('.js-rocketchat-icon').classList.remove('display-none')
-      }
-      return true;
-    }
-  }
-}
-const rocketChatProxy = new Proxy(rocketChatIframe, handler());
