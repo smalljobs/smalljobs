452e74833058b568f0934e78a5a67cb5e4c55303
Przemysław Zając <zajac@netkodo.com>
2021-04-29 23:39:39 +0200

    Correct code tranform

--- app/assets/javascripts/rocketchat_observer.js
+++ app/assets/javascripts/rocketchat_observer.js
@@ -1,21 +1,17 @@
-"use strict";
-var rocketChatIframe = {
-	rcAvailableInIframe: false,
-	iframeLoad: false,
-	url: ''
-};
+var rocketChatIframe = { rcAvailableInIframe: false, iframeLoad: false, url: '' };
 var handler = function () {
 	return {
 		set: function (target, key, value) {
 			target[key] = value;
 			if (target.rcAvailableInIframe && target.iframeLoad && window.iframeEl.src.length !== target.url.length) {
-				console.log(target);
 				window.iframeEl.src = target.url;
-				document.querySelector('.js-rocketchat-icon').classList.remove('display-none');
+				var rocketChatIcon = document.querySelector('.js-rocketchat-icon');
+				if (rocketChatIcon) {
+					rocketChatIcon.classList.remove('display-none');
+				}
 			}
 			return true;
 		}
 	};
 };
 var rocketChatProxy = new Proxy(rocketChatIframe, handler());
-
