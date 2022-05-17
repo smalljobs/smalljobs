60701d04dcf21f13e18bd71fbb995c6804d4eb99
Daniel Niedziałkowski <niedzi1g@gmail.com>
2022-05-17 11:57:52 +0200

    test commit

--- app/controllers/broker/providers_controller.rb
+++ app/controllers/broker/providers_controller.rb
@@ -47,6 +47,7 @@ class Broker::ProvidersController < InheritedResources::Base
     provider_phone = @provider.mobile.empty? ? @provider.phone : @provider.mobile
     @letter_msg = Mustache.render(@provider.organization.welcome_letter_employers_msg || '', provider_first_name: @provider.firstname, provider_last_name: @provider.lastname, provider_phone: provider_phone, broker_first_name: current_broker.firstname, broker_last_name: current_broker.lastname, organization_name: @provider.organization.name, organization_zip: @provider.organization.place.zip, organization_street: @provider.organization.street, organization_place: @provider.organization.place.name, organization_phone: @provider.organization.phone, organization_email: @provider.organization.email, link_to_jobboard_list: url_for(root_url()))
     @letter_msg.gsub! "\n", "<br>"
+    @default_text = providers_contract_text
     render pdf: 'contract', template: 'broker/providers/contract.html.erb', margin: {top: 0, left: 0, right: 0, bottom: 0}
   end
 
